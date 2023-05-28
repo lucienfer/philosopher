5a0076ced6c252b47ae2c91f7b6021e73b9717f9
Lucien FERNANDEZ <luciefer@e1r7p4.clusters.42paris.fr>
2023-05-28 16:27:57 +0200

    tets

---
+++ src/utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciefer <luciefer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 10:29:55 by luciefer          #+#    #+#             */
+/*   Updated: 2023/05/23 10:31:10 by luciefer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+unsigned long	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * (unsigned long)1000) + (time.tv_usec / 1000));
+}
+
+void	new_sleep(unsigned long duration, t_env *env)
+{
+	unsigned long	start;
+
+	start = get_time();
+	while (!env->stop_condition)
+	{
+		if (get_time() - start >= duration)
+			break ;
+		usleep(env->count * 2);
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_return_error(char *msg)
+{
+	write(2, msg, ft_strlen(msg));
+	return (0);
+}
+
+int	ft_isint(const char *nptr)
+{
+	int		i;
+	int		neg;
+	long	value;
+
+	i = 0;
+	value = 0;
+	neg = 0;
+	while ((nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13)))
+		i++;
+	if (nptr[i] == '-')
+		neg = 1;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		i++;
+	while (nptr[i] != '\0' && (nptr[i] >= 48 && nptr[i] <= 57))
+	{
+		if (value > 214748364 || (value == 214748364
+				&& ((!neg && nptr[i] - '0' > 7) || (neg && nptr[i] - '0' > 8))))
+			return (0);
+		else
+			value = (value * 10) + nptr[i++] - '0';
+	}
+	return (1);
+}
